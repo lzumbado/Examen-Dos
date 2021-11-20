8f8252c38712c5027a43eab3cea56397429f92fc
LEONARDO JOSUE ZUMBADO <leozch11@gmail.com>
2021-11-19 20:44:20 -0600

    Store Procedure Orden

---
+++ Database/Database/dbo/StoreProcedures/OrdenObtener.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[OrdenObtener]
+  @IdOrden int= NULL
+AS BEGIN
+  SET NOCOUNT ON
+
+  SELECT 
+     O.IdOrden,
+     O.IdProducto,
+     O.CantidadProducto,
+     O.Estado    
+    FROM dbo.Orden O
+    WHERE
+    (@IdOrden IS NULL OR IdOrden=@IdOrden)
+
+END
---
+++ Database/Database/dbo/StoreProcedures/ProductoLista.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[ProductoLista]
+AS
+	BEGIN
+	SET NOCOUNT ON
+
+
+	SELECT
+	 IdProducto,
+	 NombreProducto,
+	 PrecioProducto
+	FROM Producto
+
+
+	END
