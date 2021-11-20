33c9931ae644024bb6e6f14942f2e87965f2b13f
LEONARDO JOSUE ZUMBADO <leozch11@gmail.com>
2021-11-19 19:22:14 -0600

    Add project files.

---
+++ Database/Database/dbo/Tables/Producto.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Producto]
+(
+	IdProducto INT NOT NULL IDENTITY(1,1) CONSTRAINT PK_Producto PRIMARY KEY CLUSTERED(IdProducto)  
+  , NombreProducto VARCHAR(50) NOT NULL
+  , PrecioProducto INT NOT NULL
+)
