337958decf3e51766c4e4e3131812d961ebc52e2
LEONARDO JOSUE ZUMBADO <leozch11@gmail.com>
2021-11-20 19:52:26 -0600

    Mantenimiento Producto

---
+++ WebApp/WebApp/Pages/Producto/Edit.cshtml.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WBL;
+using Entity;
+
+namespace WebApp.Pages.Producto
+{
+    public class EditModel : PageModel
+    {
+        private readonly IProductoService productoService;
+
+        public EditModel(IProductoService productoService)
+        {
+            this.productoService = productoService;
+           
+        }
+
+        [BindProperty]
+        [FromBody]
+
+        public ProductoEntity Entity { get; set; } = new ProductoEntity();
+
+        [BindProperty(SupportsGet = true)]
+        public int? id { get; set; }
+
+        public async Task<IActionResult> OnGet()
+        {
+            try
+            {
+                if (id.HasValue)
+                {
+                    Entity = await productoService.GetById(new() { IdProducto = id });
+                }                
+
+                return Page();
+            }
+            catch (Exception ex)
+            {
+
+                return Content(ex.Message);
+            }
+
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+
+            try
+            {
+                var result = new DBEntity();
+                if (Entity.IdProducto.HasValue)
+                {
+                    result = await productoService.Update(Entity);
+
+
+                }
+                else
+                {
+                    result = await productoService.Create(Entity);
+
+                }
+
+                return new JsonResult(result);
+            }
+            catch (Exception ex)
+            {
+
+                return new JsonResult(new DBEntity { CodeError = ex.HResult, MsgError = ex.Message });
+            }
+
+
+        }
+
+
+    }
+}
