ab7697212d0d8bb3aea0ab210c215b72b2af7314
LEONARDO JOSUE ZUMBADO <leozch11@gmail.com>
2021-11-21 08:01:05 -0600

    Actualiza tabla Orden

--- Database/Database/dbo/Tables/Orden.sql
+++ Database/Database/dbo/Tables/Orden.sql
@@ -3,6 +3,6 @@
 	IdOrden INT NOT NULL IDENTITY(1,1) CONSTRAINT PK_Orden PRIMARY KEY CLUSTERED(IdOrden)
   , IdProducto INT NOT NULL CONSTRAINT FK_Producto_Orden FOREIGN KEY(IdProducto) REFERENCES dbo.Producto(IdProducto)
   , CantidadProducto INT NOT NULL
-  , Estado BIT NOT NULL
+  , Estado BIT  NULL
 
 )
